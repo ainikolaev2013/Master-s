44eb1da48567261623e2668386ad9537b66678f0
Alexander Nikolaev <ainikolaev@gmail.com>
2014-01-28 00:02:08 -0800

    Sampling Rate

--- EVM_Matlab/pulsecounter.m
+++ EVM_Matlab/pulsecounter.m
@@ -1,7 +1,8 @@
 function cnt = pulsecounter(Gdown_stack, cellx, celly, margin, thr)
     fl = 30/60;
     fh = 150/60;
-    samplingRate = 24;
+%    samplingRate = 24;
+    samplingRate = 25; % 50 for 3780
     filtered_stack = ideal_bandpassing(Gdown_stack, 1, fl, fh, samplingRate);
     plot(filtered_stack (:, cellx, celly, 1));
     len = length(filtered_stack (:, cellx, celly, 1));
