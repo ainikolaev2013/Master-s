d5be903ba25ab0d313bc97fbdb1a23c2d5f1a0a6
Alexander Nikolaev <ainikolaev@gmail.com>
2014-01-29 04:32:40 -0800

    NTSC + 3 Sources

--- EVM_Matlab/Maria_stack_manual.m
+++ EVM_Matlab/Maria_stack_manual.m
@@ -55,7 +55,9 @@ frame=rgbframe;
             [rgbframe,~] = frame2im(temp);
 
             rgbframe = im2double(rgbframe);
-            frame = rgb2ntsc(rgbframe);
+%    frame = rgb2ntsc(rgbframe);
+% No need for this on Canon 650D videos.
+frame=rgbframe;
 
             cropped = imcrop(frame,cropwindow);
             Stack(k,:,:,:) = cropped;
@@ -117,8 +119,9 @@ frame=rgbframe;
             [rgbframe,~] = frame2im(temp);
 
             rgbframe = im2double(rgbframe);
-            frame = rgb2ntsc(rgbframe);
-
+%    frame = rgb2ntsc(rgbframe);
+% No need for this on Canon 650D videos.
+frame=rgbframe;
             cropped = imcrop(frame,cropwindow);
             Stack(k,:,:,:) = cropped;
     end;
@@ -179,7 +182,9 @@ frame=rgbframe;
             [rgbframe,~] = frame2im(temp);
 
             rgbframe = im2double(rgbframe);
-            frame = rgb2ntsc(rgbframe);
+%    frame = rgb2ntsc(rgbframe);
+% No need for this on Canon 650D videos.
+frame=rgbframe;
 
             cropped = imcrop(frame,cropwindow);
             Stack(k,:,:,:) = cropped;
@@ -242,8 +247,9 @@ frame=rgbframe;
             [rgbframe,~] = frame2im(temp);
 
             rgbframe = im2double(rgbframe);
-            frame = rgb2ntsc(rgbframe);
-
+%    frame = rgb2ntsc(rgbframe);
+% No need for this on Canon 650D videos.
+frame=rgbframe;
             cropped = imcrop(frame,cropwindow);
             Stack(k,:,:,:) = cropped;
     end;
@@ -302,8 +308,9 @@ frame=rgbframe;
             [rgbframe,~] = frame2im(temp);
 
             rgbframe = im2double(rgbframe);
-            frame = rgb2ntsc(rgbframe);
-
+            %    frame = rgb2ntsc(rgbframe);
+% No need for this on Canon 650D videos.
+frame=rgbframe;
             cropped = imcrop(frame,cropwindow);
             Stack(k,:,:,:) = cropped;
     end;
@@ -362,14 +369,202 @@ frame=rgbframe;
             [rgbframe,~] = frame2im(temp);
 
             rgbframe = im2double(rgbframe);
-            frame = rgb2ntsc(rgbframe);
-
+            %    frame = rgb2ntsc(rgbframe);
+% No need for this on Canon 650D videos.
+frame=rgbframe;
             cropped = imcrop(frame,cropwindow);
             Stack(k,:,:,:) = cropped;
     end;
     disp(['Stack for ' filename{i} ' has been built.'])
     save(fullfile(resultsDir,[filename{i} 'rightcheek_stack.mat']), 'Stack');
 end;
+
+filename = {'3780_cut2_stab2'};
+
+for i=1:numel(filename) 
+%forehead
+x0=240;
+y0=150;
+width=50;
+height=50;
+    
+    inFile = fullfile(dataDir, [filename{i} '.avi']);
+
+fprintf('Processing %s\n', inFile);
+
+vidFile=inFile;
+
+cropwindow = [x0 y0 width height];
+[~,vidName] = fileparts(vidFile);
+
+    % Read video
+    vid = VideoReader(vidFile);
+    % Extract video info
+    vidHeight = vid.Height;
+    vidWidth = vid.Width;
+    nChannels = 3;
+    fr = vid.FrameRate;
+    len = vid.NumberOfFrames;
+    temp = struct('cdata', zeros(vidHeight, vidWidth, nChannels, 'uint8'), 'colormap', []);
+
+    startIndex(i) = 1;
+    endIndex(i) = len;
+
+    temp.cdata = read(vid, startIndex(i));
+    [rgbframe, ~] = frame2im(temp);
+    rgbframe = im2double(rgbframe);
+%    frame = rgb2ntsc(rgbframe);
+% No need for this on Canon 650D videos.
+frame=rgbframe;
+
+    % compute  stack
+    disp(['Building stack for ' filename{i}])
+
+    cropped = imcrop(frame,cropwindow);
+    % create  stack
+    Stack = zeros(endIndex(i) - startIndex(i) +1, size(cropped,1),size(cropped,2),size(cropped,3));
+    Stack(1,:,:,:) = cropped;
+
+    k = 1;
+    for j=startIndex(i)+1:endIndex(i)
+            k = k+1;
+            temp.cdata = read(vid, j);
+            [rgbframe,~] = frame2im(temp);
+
+            rgbframe = im2double(rgbframe);
+%    frame = rgb2ntsc(rgbframe);
+% No need for this on Canon 650D videos.
+frame=rgbframe;
+            cropped = imcrop(frame,cropwindow);
+            Stack(k,:,:,:) = cropped;
+    end;
+    disp(['Stack for ' filename{i} ' has been built.'])
+    save(fullfile(resultsDir,[filename{i} 'forehead_stack.mat']), 'Stack');
+end;
+
+for i=1:numel(filename) 
+%right cheek
+x0=175;
+y0=295;
+width=50;
+height=50;
+    
+    inFile = fullfile(dataDir, [filename{i} '.avi']);
+
+fprintf('Processing %s\n', inFile);
+
+vidFile=inFile;
+
+cropwindow = [x0 y0 width height];
+[~,vidName] = fileparts(vidFile);
+
+    % Read video
+    vid = VideoReader(vidFile);
+    % Extract video info
+    vidHeight = vid.Height;
+    vidWidth = vid.Width;
+    nChannels = 3;
+    fr = vid.FrameRate;
+    len = vid.NumberOfFrames;
+    temp = struct('cdata', zeros(vidHeight, vidWidth, nChannels, 'uint8'), 'colormap', []);
+
+    startIndex(i) = 1;
+    endIndex(i) = len;
+
+    temp.cdata = read(vid, startIndex(i));
+    [rgbframe, ~] = frame2im(temp);
+    rgbframe = im2double(rgbframe);
+%    frame = rgb2ntsc(rgbframe);
+% No need for this on Canon 650D videos.
+frame=rgbframe;
+
+    % compute  stack
+    disp(['Building stack for ' filename{i}])
+
+    cropped = imcrop(frame,cropwindow);
+    % create  stack
+    Stack = zeros(endIndex(i) - startIndex(i) +1, size(cropped,1),size(cropped,2),size(cropped,3));
+    Stack(1,:,:,:) = cropped;
+
+    k = 1;
+    for j=startIndex(i)+1:endIndex(i)
+            k = k+1;
+            temp.cdata = read(vid, j);
+            [rgbframe,~] = frame2im(temp);
+
+            rgbframe = im2double(rgbframe);
+            %    frame = rgb2ntsc(rgbframe);
+% No need for this on Canon 650D videos.
+frame=rgbframe;
+            cropped = imcrop(frame,cropwindow);
+            Stack(k,:,:,:) = cropped;
+    end;
+    disp(['Stack for ' filename{i} ' has been built.'])
+    save(fullfile(resultsDir,[filename{i} 'leftcheek_stack.mat']), 'Stack');
+end;
+
+for i=1:numel(filename) 
+%left cheek
+x0=310;
+y0=290;
+width=50;
+height=50;
+    
+    inFile = fullfile(dataDir, [filename{i} '.avi']);
+
+fprintf('Processing %s\n', inFile);
+
+vidFile=inFile;
+
+cropwindow = [x0 y0 width height];
+[~,vidName] = fileparts(vidFile);
+
+    % Read video
+    vid = VideoReader(vidFile);
+    % Extract video info
+    vidHeight = vid.Height;
+    vidWidth = vid.Width;
+    nChannels = 3;
+    fr = vid.FrameRate;
+    len = vid.NumberOfFrames;
+    temp = struct('cdata', zeros(vidHeight, vidWidth, nChannels, 'uint8'), 'colormap', []);
+
+    startIndex(i) = 1;
+    endIndex(i) = len;
+
+    temp.cdata = read(vid, startIndex(i));
+    [rgbframe, ~] = frame2im(temp);
+    rgbframe = im2double(rgbframe);
+%    frame = rgb2ntsc(rgbframe);
+% No need for this on Canon 650D videos.
+frame=rgbframe;
+
+    % compute  stack
+    disp(['Building stack for ' filename{i}])
+
+    cropped = imcrop(frame,cropwindow);
+    % create  stack
+    Stack = zeros(endIndex(i) - startIndex(i) +1, size(cropped,1),size(cropped,2),size(cropped,3));
+    Stack(1,:,:,:) = cropped;
+
+    k = 1;
+    for j=startIndex(i)+1:endIndex(i)
+            k = k+1;
+            temp.cdata = read(vid, j);
+            [rgbframe,~] = frame2im(temp);
+
+            rgbframe = im2double(rgbframe);
+            %    frame = rgb2ntsc(rgbframe);
+% No need for this on Canon 650D videos.
+frame=rgbframe;
+            cropped = imcrop(frame,cropwindow);
+            Stack(k,:,:,:) = cropped;
+    end;
+    disp(['Stack for ' filename{i} ' has been built.'])
+    save(fullfile(resultsDir,[filename{i} 'rightcheek_stack.mat']), 'Stack');
+end;
+
+
 
 disp('Stacks have been built.')
 
