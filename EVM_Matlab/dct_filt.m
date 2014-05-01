29ccfa287cfb2ac7573ac9b6da5073e436d08108
Alexander Nikolaev <ainikolaev@gmail.com>
2014-05-02 03:19:35 +0400

    More info on what's going on

--- EVM_Matlab/dct_filt.m
+++ EVM_Matlab/dct_filt.m
@@ -2,8 +2,16 @@
 %K should be chosen carefully
 function dct_filtres=dct_filt(stack)
 K=0.01
+
+%performing DCT
+disp('Performing the DCT...')
 dct_stack=dct(stack);
+
+%Zeroing out all values less than K
 dct_stack(abs(dct_stack) < K) = 0;
+
+%restoring the stack
+disp('Restoring the signal...')
 dct_filtres=idct(dct_stack);
 
 end
