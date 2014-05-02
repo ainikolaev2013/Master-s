ae545e6d784b3f4a3eb8a6ea3b6e3bf5f6397efa
Alexander Nikolaev <ainikolaev@gmail.com>
2014-05-03 03:08:16 +0400

    Making sense of the videos

---
+++ EVM_Matlab/Test16LineStab.m
@@ -0,0 +1,27 @@
+%stabilize 16 line videos
+dataDir = 'E:\Grad School\Master''s\CanonVideos\16 line test\cuts\K';
+
+list=dir ('E:\Grad School\Master''s\CanonVideos\16 line test\cuts\K\*.mp4');
+
+
+resultsDir='./output/16/stab';
+mkdir(resultsDir);
+
+%????? they have to be switched around
+intervalx = [150:850];
+%intervaly = [70:750];
+intervaly = [70:700];
+
+%failed earlier
+for iterator =6:numel(list)
+    tic
+disp (list(iterator).name);
+
+vidFile=fullfile(dataDir, list(iterator).name);
+[~,vidName] = fileparts(vidFile);
+outName=fullfile(resultsDir, [vidName '_stab.mp4']);
+
+
+stabilize(vidFile, intervalx, intervaly, outName);
+toc
+end;
