2d60bdb57d9fc09972986523ea9c22f74920a281
Alexander Nikolaev <ainikolaev@gmail.com>
2014-05-25 22:02:33 +0400

    Trivialities

--- EVM_Matlab/Add_noise_stacks.m
+++ EVM_Matlab/Add_noise_stacks.m
@@ -19,19 +19,21 @@ load(StackFile, 'Stack');
 Stack_work=Stack;
 
 
-for sigma = 0.002:0.011:0.221
+for sigma =  0.001:0.004:0.013
 %sigma
 noisy_stack=add_noise(Stack_work, sigma);
 Stack=noisy_stack;
+
 
 save_loc_cell=fullfile(resultsDir, strcat(Name(1), '_', Name(2), '_', Name(3), '_', strrep(num2str(sigma),'.',''), '.mat'));
 save_loc=save_loc_cell{1}
 
 save(save_loc, 'Stack');
 
+
 end;
 
-for sigma = 0.001:0.004:0.013
+for sigma = 0.002:0.011:0.221
 %sigma
 noisy_stack=add_noise(Stack_work, sigma);
 Stack=noisy_stack;
