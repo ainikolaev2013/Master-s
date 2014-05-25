bdc55aca9bd9913118a1c3b488396a5b5524e2d5
Alexander Nikolaev <ainikolaev@gmail.com>
2014-05-25 17:36:56 +0400

    All noise levels at once

--- EVM_Matlab/Add_noise_stacks.m
+++ EVM_Matlab/Add_noise_stacks.m
@@ -31,8 +31,29 @@ save(save_loc, 'Stack');
 
 end;
 
+for sigma = 0.001:0.004:0.013
+%sigma
+noisy_stack=add_noise(Stack_work, sigma);
+Stack=noisy_stack;
 
+save_loc_cell=fullfile(resultsDir, strcat(Name(1), '_', Name(2), '_', Name(3), '_', strrep(num2str(sigma),'.',''), '.mat'));
+save_loc=save_loc_cell{1}
 
+save(save_loc, 'Stack');
+
+end;
+
+for sigma = 0.001:0.050:2.051
+%sigma
+noisy_stack=add_noise(Stack_work, sigma);
+Stack=noisy_stack;
+
+save_loc_cell=fullfile(resultsDir, strcat(Name(1), '_', Name(2), '_', Name(3), '_', strrep(num2str(sigma),'.',''), '.mat'));
+save_loc=save_loc_cell{1}
+
+save(save_loc, 'Stack');
+
+end;
 
 end;
 
