33206d880a3fa1dc2231d00794c5b85f9f423c6d
Alexander Nikolaev <ainikolaev@gmail.com>
2014-05-25 09:52:31 +0400

    Input ideal bandpassing

--- EVM_Matlab/run_tests_general.m
+++ EVM_Matlab/run_tests_general.m
@@ -43,7 +43,7 @@ K= 0.006;
 %MIT parameters
 samplingRate=30;
 fl=40/60;
-fh=70/60;
+fh=80/60;
 %OMP K_target value
 K_target=5;
 %determines saving location
@@ -52,77 +52,77 @@ elseif strcmp(Record(1),'6351') && strcmp(Record(2),'cut1')
 K= 0.007;
 samplingRate=25;
 fl=40/60;
-fh=70/60;
+fh=80/60;
 K_target=7;
 record_type='hand';
 elseif strcmp(Record(1),'6351') && strcmp(Record(2),'cut2')
 K= 0.007;
 samplingRate=25;
 fl=40/60;
-fh=70/60;
+fh=80/60;
 K_target=7;
 record_type='hand';
 elseif strcmp(Record(1),'6353') && strcmp(Record(2),'cut1')
 K= 0.015;
 samplingRate=25;
 fl=40/60;
-fh=70/60;
+fh=80/60;
 K_target=40;
 record_type='hand';
 elseif strcmp(Record(1),'6353') && strcmp(Record(2),'cut2')
 K= 0.015;
 samplingRate=25;
 fl=40/60;
-fh=70/60;
+fh=80/60;
 K_target=40;
 record_type='hand';
 elseif strcmp(Record(1),'6355') && strcmp(Record(2),'cut1')
 K= 0.015;
 samplingRate=25;
 fl=40/60;
-fh=70/60;
+fh=80/60;
 K_target=9;
 record_type='hand';
 elseif strcmp(Record(1),'6355') && strcmp(Record(2),'cut2')
 K= 0.015;
 samplingRate=25;
 fl=40/60;
-fh=70/60;
+fh=80/60;
 K_target=9;
 record_type='hand';
 elseif strcmp(Record(1),'6356') && strcmp(Record(2),'cut1')
 K= 0.015;
 samplingRate=25;
 fl=40/60;
-fh=70/60;
+fh=80/60;
 K_target=9;
 record_type='hand';
 elseif strcmp(Record(1),'6356') && strcmp(Record(2),'cut2')
 K= 0.015;
 samplingRate=25;
 fl=40/60;
-fh=70/60;
+fh=80/60;
 K_target=9;
 record_type='hand';
 elseif strcmp(Record(1),'6357') && strcmp(Record(2),'cut1')
 K= 0.015;
 samplingRate=25;
 fl=40/60;
-fh=70/60;
+fh=80/60;
 K_target=13;
 record_type='hand';
 elseif strcmp(Record(1),'6357') && strcmp(Record(2),'cut2')
 K= 0.015;
 samplingRate=25;
 fl=40/60;
-fh=70/60;
+fh=80/60;
 K_target=13;
 record_type='hand';
 else
 K= 0.006;
 samplingRate=25;
 fl=40/60;
-fh=70/60;
+fh=80/60;
 K_target=5;
 record_type='hand';
 end
@@ -131,6 +131,8 @@ end
 level=4;  %64x64 input stacks!
 disp('Spacial filtering...')
 Stack1x1 = build_GDown_stack_stack4(StackFile, level);
+Stack1x1 = ideal_bandpassing(Stack1x1, 1, 30/60, 300/60, samplingRate);
+
 disp('Finished filtering...')
 
 %looking into the first channel
@@ -153,7 +155,7 @@ dct_stack=dct_filt(Stack1, K);
 disp('DCT filtering done.');
 
 dct_result=length(findpeaks(dct_stack));
-Results_run=[Results_run; runID  Record(1) Record(3) Record(4) 'DCT' num2str(K) dct_result];
+Results_run=[Results_run; runID  Record(1) Record(3) Record(4) 'DCT 5Hz' num2str(K) dct_result];
 
 end
 
@@ -163,15 +165,21 @@ disp('DCT finished.');
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %MIT algorithm
+for low = 30:10:50
+    for high = 70:10:100
 disp('MIT algorithm...');
 
+fl=low/60;
+fh=high/60;
 disp('Temporal filtering...')
 MIT_stack = ideal_bandpassing(Stack1, 1, fl, fh, samplingRate);
 disp('Temporal filtering done.')
 
 MIT_result=length(findpeaks(MIT_stack));
-Results_run=[Results_run; runID  Record(1) Record(3) Record(4) 'MIT' 'n/a' MIT_result];
+Results_run=[Results_run; runID  Record(1) Record(3) Record(4) 'MIT 5Hz' strcat(num2str(fl*60), '-', num2str(fh*60)) MIT_result];
 disp('MIT finished.');
+    end
+end
 
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
@@ -189,7 +197,7 @@ disp('JADE filtering done.');
 
 
 JADE_result=length(findpeaks(JADE_stack));
-Results_run=[Results_run; runID  Record(1) Record(3) Record(4) 'JADE' 'n/a' JADE_result];
+Results_run=[Results_run; runID  Record(1) Record(3) Record(4) 'JADE 5Hz' 'n/a' JADE_result];
 disp('JADE finished.');
 
 
@@ -207,7 +215,7 @@ OMP_stack=filter_sparse_noiterator(Stack1, K_target);
 disp('OMP filtering done.');
 
 OMP_result=length(findpeaks(OMP_stack));
-Results_run=[Results_run; runID  Record(1) Record(3) Record(4) 'OMP' num2str(K_target) OMP_result];
+Results_run=[Results_run; runID  Record(1) Record(3) Record(4) 'OMP 5Hz' num2str(K_target) OMP_result];
 end
 
 
