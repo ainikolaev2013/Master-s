8eb0710b5325d70d60d49df75392977b163f8a5c
Alexander Nikolaev <ainikolaev@gmail.com>
2014-05-25 05:21:38 +0400

    Better noise creation

--- EVM_Matlab/Add_noise_stacks.m
+++ EVM_Matlab/Add_noise_stacks.m
@@ -1,7 +1,8 @@
 %add specific amounts of noise to the stacks in the folder
-dataDir = './data/face';
+tic
+dataDir = './data/hand/';
 list=dir([dataDir '\*.mat']);
-resultsDir='f:\spbSU\masters\evM_Matlab\output\face\noisy';
+resultsDir='f:\spbSU\masters\evM_Matlab\output\hand\noisy';
 
 
 
@@ -18,15 +19,21 @@ load(StackFile, 'Stack');
 Stack_work=Stack;
 
 
-for sigma = 0.001:0.004:0.013
+for sigma = 0.002:0.011:0.221
 %sigma
 noisy_stack=add_noise(Stack_work, sigma);
 Stack=noisy_stack;
-save(fullfile(resultsDir, strcat(file, '_test_stack1_', strrep(num2str(sigma),'.',''), '.mat')), 'Stack');
+
+save_loc_cell=fullfile(resultsDir, strcat(Name(1), '_', Name(2), '_', Name(3), '_', strrep(num2str(sigma),'.',''), '.mat'));
+save_loc=save_loc_cell{1}
+
+save(save_loc, 'Stack');
 
 end;
 
 
 
- 
+
 end;
+
+toc
