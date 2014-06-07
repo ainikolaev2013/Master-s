1ba897c23fa3dd15612c2fe0d71affc9fed4dec8
Alexander Nikolaev <ainikolaev@gmail.com>
2014-06-07 17:46:43 +0400

    Good sin test

--- EVM_Matlab/gen_sin.m
+++ EVM_Matlab/gen_sin.m
@@ -1,21 +1,21 @@
    %% Time specifications:
    Fs = 25;                   % samples per second
    dt = 1/Fs;                   % seconds per sample
-   StopTime = 2;             % seconds
+   StopTime = 1000;             % seconds
    t = (0:dt:StopTime-dt)';     % seconds
    sigma=1              %noise
  
    %% Sine wave:
-   Fc = 60/60;                     % hertz
+   Fc = 90/60;                     % hertz
    %x = sin(2*pi*Fc*t);
    random_d= rand(size(t))-0.5;
-   x = sin(2*pi*Fc*t)% +  sigma*random_d(t)  %add noise
+   x = sin(2*pi*Fc*t);% +  sigma*random_d(t)  %add noise
    for iterator = 1:size(x)
-       x=x+sigma*random_d(iterator);
+       x(iterator)=x(iterator)+sigma*random_d(iterator);
    end
    % Plot the signal versus time:
    figure;
    plot(t,x);
    xlabel('time (in seconds)');
    title('Signal versus Time');
-   Stack=[(1:iterator)' t];
+  % Stack=[(1:iterator)' t];
